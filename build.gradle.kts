842a431782c2f5b59aee253bc37c1cc8e11fddb7
gektor650 <gektor650@gmail.com>
2024-03-20 10:08:29 -0600

    Fix text formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
     version.set("2022.2.5")
+    type.set("IU")
 }
 
 //includeTransitiveDependencies = true
