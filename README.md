312cc4b5cc7d481cb9a1221ca4ff04402600c3d5
gektor650 <gektor650@gmail.com>
2024-03-20 21:22:06 -0600

    Fix readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# ai-test-creator
+# ChatGPT Assistant for IntelliJ IDEA
+
+![Screenshot1](/images/image1.png)
+
+![Screenshot2](/images/image2.png)
+
+This plugin  makes ChatGPT request for you and paste the answers directly into the code.
+No needs to copy code to the browser and back, just ask GPT from the IDE and see the answer in a moment.
+
+## How It Works
+
+*  Go to the OpenAI site and create API key: <a href="https://platform.openai.com/api-keys">https://platform.openai.com/api-keys</a>.
+* In IDE open a Tools menu and press <b>AskGpt Settings</b>. Paste the ChatGPT API key into the <b>GPT Token</b> field (the first one).
+
+To call the plugin just press *Control+Enter* shortcut in the code.
+A context-sensitive menu will appear, allowing you to select a task.
+The plugin then communicates with ChatGPT and provides you with actionable solutions or suggestions, right where you need them.
+
+## Key Features
+* **Custom Asks**: Query ChatGPT directly to generate custom code snippets, algorithms, or tackle programming challenges right within your IDE.</li>
+* **Write Tests**: Automatically generate test cases for your code. Simply specify the target, and let ChatGPT Assistant create comprehensive tests to ensure reliability.</li>
+* **Find Bugs**: Describe your bug to ChatGPT Assistant, and it will help identify potential flaws in your code, suggest debugging steps, or offer direct solutions.</li>
+* **Create Docs**: Generate documentation effortlessly. ChatGPT Assistant can document your functions, classes, and modules, making your codebase easier to navigate and maintain.</li>
--- src/main/kotlin/io/nerdythings/dialog/SettingsDialog.kt
+++ src/main/kotlin/io/nerdythings/dialog/SettingsDialog.kt
@@ -5,6 +5,9 @@ import com.intellij.ui.components.JBScrollPane
 import com.intellij.util.ui.JBUI
 import io.nerdythings.preferences.AppSettingsState
 import java.awt.*
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+import java.net.URI
 import javax.swing.*
 import javax.swing.event.DocumentListener
 
@@ -18,13 +21,25 @@ class SettingsDialog : DialogWrapper(true) {
 
     override fun createCenterPanel(): JComponent? {
         contentPane = JPanel().apply {
-            setLayout(GridLayout(5, 1))
-
-            addLabeledTextField(this, "ChatGPT token", AppSettingsState.instance.gptToken.orEmpty(), textRows = 1) {
+            setLayout(GridLayout(6, 1))
+            createTextLink(
+                this,
+                "ChatGPT token (click here to create)",
+                "https://platform.openai.com/api-keys",
+                AppSettingsState.instance.gptToken.orEmpty(),
+            ) {
                 AppSettingsState.instance.gptToken = it
             }
-            addLabeledTextField(this, "ChatGPT model", AppSettingsState.instance.gptModel, textRows = 1) {
+            createTextLink(
+                this,
+                "ChatGPT model (click here to choose)",
+                "https://platform.openai.com/account/limits",
+                AppSettingsState.instance.gptModel,
+            ) {
                 AppSettingsState.instance.gptModel = it
+            }
+            addLabeledTextField(this, "ChatGPT Default Question", AppSettingsState.instance.gptAsk, textRows = 1) {
+                AppSettingsState.instance.gptAsk = it
             }
             addLabeledTextField(this, "ChatGpt Test Question", AppSettingsState.instance.createTestQuestion) {
                 AppSettingsState.instance.createTestQuestion = it
@@ -80,6 +95,54 @@ class SettingsDialog : DialogWrapper(true) {
         panel.add(Box.createVerticalStrut(5))
         panel.add(scrollPane)
         panel.add(Box.createVerticalStrut(20))
+        parentPanel.add(panel)
+    }
+
+    private fun createTextLink(
+        parentPanel: JPanel,
+        labelText: String,
+        linkURL: String,
+        textFieldText: String,
+        onChange: (String) -> Unit
+    ) {
+        val panel = JPanel().apply {
+            setLayout(GridLayout(2, 1))
+        }
+
+        val textArea = JTextField(textFieldText, 100)
+
+        textArea.document.addDocumentListener(object : DocumentListener {
+            override fun insertUpdate(e: javax.swing.event.DocumentEvent?) {
+                onChange.invoke(textArea.text)
+            }
+
+            override fun removeUpdate(e: javax.swing.event.DocumentEvent?) {
+                onChange.invoke(textArea.text)
+            }
+
+            override fun changedUpdate(e: javax.swing.event.DocumentEvent?) {
+                onChange.invoke(textArea.text)
+            }
+        })
+
+        textArea.margin = JBUI.insets(10)
+        val linkLabel = JLabel(labelText)
+        linkLabel.foreground = Color.BLUE.darker()
+        linkLabel.cursor = Cursor(Cursor.HAND_CURSOR)
+        linkLabel.toolTipText = linkURL
+        linkLabel.text = "<html><a href=''>" + linkLabel.text + "</a></html>"
+
+        linkLabel.addMouseListener(object : MouseAdapter() {
+            override fun mouseClicked(e: MouseEvent?) {
+                try {
+                    Desktop.getDesktop().browse(URI(linkURL))
+                } catch (ex: Exception) {
+                    ex.printStackTrace()
+                }
+            }
+        })
+        panel.add(linkLabel)
+        panel.add(textArea)
         parentPanel.add(panel)
     }
 }
